59fad5da81b3d0cd67d9e2361b6f8b8c4e598ae9
Marcus Renno <me@rennomarcus.com>
2019-03-17 19:31:00 -0400

    Add RT checking and added more logs

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import tweepy
 import random
 import time
 
+def getTime():
+    return time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
 
 # Access and authorize our Twitter credentials from credentials.py
 class Bot():
@@ -19,16 +21,29 @@ class Bot():
 
     # make search based on our favorite hash tags
     def queryTags(self, hash_tags=HASH_TAGS):
+        print("Querying twitter at {0}".format(getTime()))
         for tag in hash_tags:
             for tweet in tweepy.Cursor(self.api.search, q=tag).items(5):
                 self.saveTweet(tweet)
 
     # save tweets
     def saveTweet(self, tweet):
-        # check first if tweet is not in our DB
-        if self.db.getTweet(tweet.id):
+        print("Checking {0}".format(tweet.id_str))
+        tweet_checked = tweet
+
+        # check if tweet is a RT. If it is we need to get it, because the tweet ids
+        # from the RT and the tweet  are different and we only care about the original one
+        if (hasattr(tweet, 'retweeted_status')):
+            print("Tweet is an RT. OG: {0} ".format(tweet.retweeted_status.id_str))
+            tweet_checked = tweet.retweeted_status
+
+        # check if tweet is not in our DB
+        if self.db.getTweet(tweet_checked.id):
+            print("Tweet already stored in DB")
             return
-        self.db.saveTweet("tweets", tweet)
+
+        print("Storing {0}".format(tweet.id_str))
+        self.db.saveTweet("tweets", tweet_checked)
 
     # create analysis of latests tweets
     def createAnalysis(self):
@@ -41,15 +56,16 @@ class Bot():
                     {"retweet_count": {"$gte": MIN_RETWEETS}},
                     {"favorite_count": {"$gte": MIN_FAVS}}
                 ]
-            }).sort("created_at", DSC).limit(3)
+            }).sort("created_at", DSC).limit(10)
 
     # retweet based on analysis
     def retweet(self):
-        print("Retweeting...")
+        print("Retweeting at {0}".format(getTime()))
         for tweet in self.getGoodTweets():
             # first we checked if we already retweeted it
             if self.db.getMyTweet(tweet["status_id"]):
                 continue
+            print("Retweeting {0}".format(tweet["status_id"]))
             self.db.saveTweet("my_tweets", tweet, False)
             # sleep for random time before tweeting
             time.sleep(random.randrange(0, 60))
